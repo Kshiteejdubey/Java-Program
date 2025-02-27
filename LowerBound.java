78cef25f5f8587f40f82f691363ebaf28c54707b
kshiteej dubey <kddubey8770@gmail.com>
2025-02-27 22:06:18 +0530

    Star pattern

--- LowerBound.java
+++ LowerBound.java
@@ -1,7 +1,7 @@
 public class LowerBound {
 
     public static void main(String[]args){
-           int[] arr ={2,3,5,6,6,7,9};
+           int[] arr ={2,3,6,5,6,6,7,9};
             int  n =6;
              
             for (int i = 0; i < arr.length; i++) {
